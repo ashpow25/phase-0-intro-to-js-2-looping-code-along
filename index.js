79d92c5c621e6e5e0ae10cfed09a370cb245ffd1
Ashley <ashpow25@gmail.com>
2021-07-11 23:08:00 -0700

    commit tree c65577e6ba33c9ed444ef1efca4ea89303b1cc13

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+//writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
 
-for (let countup = 0; countup < 10; countup++) {
-    console.log(countup);
-  } 
+//for (let countup = 0; countup < 10; countup++) {
+   // console.log(countup);
+ // }
+ const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+ function writeCards(names, event) {
+   let birthday =[];
+   event = "surprise";
+   for (let i= 0; i < names.length; i++) {
+   birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!';
+   }
+   return birthday;
+ }
+ 
+writeCards(names, birthday);
